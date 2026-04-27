da20086bd4127482f0701a777123b421e2578335
Bendik Aagaard Lynghaug <bendik.lynghaug@gmail.com>
2026-04-27 18:27:32 +0200

    chore: push flathub sync to branch, not master

--- scripts/flatpak-sync.sh
+++ scripts/flatpak-sync.sh
@@ -21,10 +21,12 @@ cp app/data/io.github.bendik.Zodia.metainfo.xml "${FLATHUB}/"
 python3 flatpak/flatpak-cargo-generator.py Cargo.lock \
   -o "${FLATHUB}/cargo-sources.json"
 
-# 4. Commit + push submodule to Flathub remote (default branch: master)
+# 4. Commit + push submodule to a branch on Flathub remote (master is protected).
+# Open a PR at: https://github.com/flathub/io.github.bendik.Zodia/pull/new/update/${TAG}
+BRANCH="update/${TAG}"
 git -C "${FLATHUB}" add -A
 git -C "${FLATHUB}" commit -m "bump to ${TAG}"
-git -C "${FLATHUB}" push origin HEAD:master
+git -C "${FLATHUB}" push origin "HEAD:refs/heads/${BRANCH}"
 
 # 5. Record the updated submodule pointer in zodia
 git add "${FLATHUB}"
